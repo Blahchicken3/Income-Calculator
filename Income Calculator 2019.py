04aee00b77ddb27182c8b29b6485673afc0597ff
Jacob Slusser <jslusser33@gmail.com>
2019-04-02 14:47:11 -0500

    Commented out currently unused, and testing with Indiana

--- Income Calculator 2019.py
+++ Income Calculator 2019.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python3
 
 import State
-import Fed
+#import Fed
 
-income = input('Do you get paid hourly or salary? ')
+Ptype = input('Do you get paid hourly or salary? ')
+STnum = str(input('Do you live in the same state that you work in? '))
 
-if income == "hourly" or 'Hourly':
-    stnum = input('Do you live in the same state that you work in? ')
-    if stnum == 'Yes' or 'yes':
-        stone = input('What state do you live in? ')
-        sttwo = input('What state do you work in? ')
-    else:
-        stone = input('What state is this for? ')
-        
+if STnum in ['Yes', 'yes']:
+    STone = (input('What state do you live in? (Examples: IN, IL, AK) '))
+#else:
+#    STone = (input('What state do you live in? (Examples: IN, IL, AK) '))
+#    STtwo = (input('What state do you work in? (Examples: IN, IL, AK) '))
+
+#TESTING IF IT WORKS WITH INDIANA
+if STone == 'IN':
+    STone = (100-State.IN)/100
+    print(STone)
+
+#if Ptype in ['hourly', 'Hourly']:
+
+if Ptype in ['Salary', 'salary']:
+    PayRate = float(input('How much do you make per year?: '))
+
+    if STnum in ['Yes', 'yes']:
+        TotalPayW = (PayRate * STone) / 52
+        TotalPayB = (PayRate * STone) / 26
+        TotalPayM = (PayRate * STone) / 12
+        TotalPayBY = (PayRate * STone)
+        print('Total weekly pay is '+str(round(TotalPayW,2)))
+        print('Total bi-weekly pay is '+str(round(TotalPayB,2)))
+        print('Total monthly pay is '+str(round(TotalPayM,2))) 
+        exit()
+#    else:
+  
