ee0cfae91ea956eb1a6bdc30d781edf75bffc292
Jacob Slusser <jslusser33@gmail.com>
2019-04-04 18:56:31 -0500

    A few small changes to terms used, as well as seperating the outputs dependant on how often user is paid

--- Income Calculator 2019.py
+++ Income Calculator 2019.py
@@ -5,6 +5,7 @@ import State
 #import Fed
 
 Ptype = input('Do you get paid hourly or salary? ')
+Freq = input('How often do you get paid; Weekly, Bi-Weekly, Monthly, Quarterly, Semi-Annually, Annualy?: ')
 STnum = str(input('Do you live in the same state that you work in? '))
 
 if STnum in ['Yes', 'yes']:
@@ -12,7 +13,7 @@ if STnum in ['Yes', 'yes']:
 else:
     STone = (input('What state do you live in? (Examples: IN, IL, AK) '))
     STtwo = (input('What state do you work in? (Examples: IN, IL, AK) '))
-
+    
 ##Set variable per state
 #Indiana
 if STone in ['IN', 'in']:
@@ -24,7 +25,7 @@ if STnum in ['No', 'no'] and STtwo in ['IN', 'in']:
 #Will use this if you select "Hourly"
 if Ptype in ['hourly', 'Hourly']:
     PayRate = float(input('How much do you make an hour?: '))
-    Hours = float(input('How many hours do you work in one week?: (Overtime not calculated yet) '))
+    Hours = float(input('How many hours do you work per week?: (Overtime not calculated yet) '))
     if STnum in ['Yes', 'yes']:
         TotalPayW = (PayRate * Hours * STone) 
         TotalPayB = (PayRate * Hours * STone) * 2
@@ -32,13 +33,6 @@ if Ptype in ['hourly', 'Hourly']:
         TotalPayQ = (PayRate * Hours * STone) * 13
         TotalPaySA = (PayRate * Hours * STone) * 26
         TotalPayA = (PayRate * Hours * STone) * 52
-        print('Total weekly pay is '+str(round(TotalPayW,2)))
-        print('Total bi-weekly pay is '+str(round(TotalPayB,2)))
-        print('Total monthly pay is '+str(round(TotalPayM,2))) 
-        print('Total quarterly pay is '+str(round(TotalPayQ,2)))
-        print('Total semi-annually pay is '+str(round(TotalPaySA,2)))
-        print('Total annual pay is '+str(round(TotalPayA,2)))
-        exit()
     elif STnum in ['No', 'no']:
         TotalPayW = (PayRate * Hours * (STone+STtwo)) 
         TotalPayB = (PayRate * Hours * (STone+STtwo)) * 2
@@ -46,13 +40,6 @@ if Ptype in ['hourly', 'Hourly']:
         TotalPayQ = (PayRate * Hours * (STone+STtwo)) * 13
         TotalPaySA = (PayRate * Hours * (STone+STtwo)) * 26
         TotalPayA = (PayRate * Hours * (STone+STtwo)) * 52
-        print('Total weekly pay is '+str(round(TotalPayW,2)))
-        print('Total bi-weekly pay is '+str(round(TotalPayB,2)))
-        print('Total monthly pay is '+str(round(TotalPayM,2)))
-        print('Total quarterly pay is '+str(round(TotalPayQ,2)))
-        print('Total semi-annually pay is '+str(round(TotalPaySA,2)))
-        print('Total annual pay is '+str(round(TotalPayA,2)))
-        exit()
 
 #Will use this if you select "Salary"
 if Ptype in ['Salary', 'salary']:
@@ -66,13 +53,6 @@ if Ptype in ['Salary', 'salary']:
         TotalPayQ = (PayRate * STone) / 4
         TotalPaySA = (PayRate * STone) / 2
         TotalPayA = (PayRate * STone)
-        print('Total weekly pay is '+str(round(TotalPayW,2)))
-        print('Total bi-weekly pay is '+str(round(TotalPayB,2)))
-        print('Total monthly pay is '+str(round(TotalPayM,2))) 
-        print('Total quarterly pay is '+str(round(TotalPayQ,2)))
-        print('Total semi-annually pay is '+str(round(TotalPaySA,2)))
-        print('Total annual pay is '+str(round(TotalPayA,2)))
-        exit()
     elif STnum in ['No', 'no']:
         TotalPayW = (PayRate * (STone+STtwo)) / 52
         TotalPayB = (PayRate * (STone+STtwo)) / 26
@@ -80,10 +60,18 @@ if Ptype in ['Salary', 'salary']:
         TotalPayQ = (PayRate * (STone+STtwo)) /4
         TotalPaySA = (PayRate * (STone+STtwo)) / 2
         TotalPayA = (PayRate * (STone+STtwo))
-        print('Total weekly pay is '+str(round(TotalPayW,2)))
-        print('Total bi-weekly pay is '+str(round(TotalPayB,2)))
-        print('Total monthly pay is '+str(round(TotalPayM,2))) 
-        print('Total quarterly pay is '+str(round(TotalPayQ,2)))
-        print('Total semi-annually pay is '+str(round(TotalPaySA,2)))
-        print('Total annual pay is '+str(round(TotalPayA,2)))
-        exit()
+
+#Output what total pay would be dependant on how often user gets paid
+if Freq in ['Weekly', 'weekly']:
+    print('Total weekly pay is '+str(round(TotalPayW,2)))
+elif Freq in ['Bi-Weekly', 'bi-weekly', 'Bi-weekly', 'bi-Weekly']:
+    print('Total bi-weekly pay is '+str(round(TotalPayB,2)))
+elif Freq in ['Monthly', 'monthly']:
+    print('Total monthly pay is '+str(round(TotalPayM,2))) 
+elif Freq in ['Quarterly', 'quarterly']:
+    print('Total quarterly pay is '+str(round(TotalPayQ,2)))
+elif Freq in ['Semi-Annually', 'Semi-annually', 'semi-Annually', 'semi-annually']:
+    print('Total semi-annually pay is '+str(round(TotalPaySA,2)))
+elif Freq in ['Annually', 'annually']:
+    print('Total annual pay is '+str(round(TotalPayA,2)))
+
