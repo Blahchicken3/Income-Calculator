bd8906769e67217b2195bee30c8fd094f5a413ff
Jacob Slusser <jslusser33@gmail.com>
2019-04-23 10:10:21 -0500

    Added and changed a few values

--- Fed.py
+++ Fed.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env pythond3
 
+First = .10
+Second = .12
+Third = .22
+Fourth = .24
+Fifth = .32
+Sixth = .35
+Seventh = .37
+
+BRone = 9525
+BRtwo = 38700
+BRthree = 82500
+BRfour = 157500
+BRfive = 200000
+BRsix = 500000
+
+Oasdi = 0.062
+
+OasdiMax = 128400
+
+Medi = 0.0145
